5f79a802802892125c408deb9fa7712ced61bb7e
blarth <jminocente@gmail.com>
2022-08-04 12:34:34 -0300

    fix: all duels

--- src/services/duelUserService.ts
+++ src/services/duelUserService.ts
@@ -4,8 +4,8 @@ import * as duelUserRepository from "../repositories/duelUserRepository.js"
 export async function findAllGivenId(id : number){
   const duels = await duelUserRepository.findAllGivenId(id)
   const duelsUpcoming = duels.filter(el => el.duel.match.result === 0)
-  const duelsLost = duels.filter(el => el.duel.match.result === el.team.id)
-  const duelsWon = duels.filter(el => el.duel.match.result !== el.team.id && el.duel.match.result !== 0)
+  const duelsLost = duels.filter(el => el.duel.match.result !== el.team.id && el.duel.match.result !== 0)
+  const duelsWon = duels.filter(el => el.duel.match.result === el.team.id && el.duel.match.result !== 0)
   
   const duelsMapped = {
       duelsUpcoming,
@@ -17,6 +17,6 @@ export async function findAllGivenId(id : number){
 
 export async function findAll(){
   const duels = await duelUserRepository.findAll()
-  const duelsFiltered = duels.filter(el => el.duelUser.length < 2)
+  const duelsFiltered = duels.filter(el => el.duelUser.length < 2 && el.match.finishedAt === null)
   return duelsFiltered
 }
