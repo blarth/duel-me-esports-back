1d63bb84abf70c113e4c6007bbb276347600adf7
blarth <jminocente@gmail.com>
2022-11-06 13:31:11 -0300

    fix: filter with unique data

--- src/services/matchesService.ts
+++ src/services/matchesService.ts
@@ -5,9 +5,9 @@ export async function get(){
   const tournaments = await matchesRepository.findAll()
   return tournaments.filter(tournament => tournament.Matches.length > 0 )
 }
-export async function getByUniqueId(id){
+export async function getByUniqueId(id : number){
   const tournaments = await matchesRepository.findUniqueIdTournament(id)
-  return tournaments.filter(tournament => tournament.Matches.length > 0 )
+  return tournaments
 }
 export async function getMatchInfoForDuel(id : number){
   const match = await matchesRepository.findUniqueById(id)
