1047047ccc68c2a19de4c7698a9bed95c9dddd76
Joao Marcos Inocente Pavao <jminocente@gmail.com>
2022-11-06 12:51:16 -0300

    fix:router getUniqueById

--- src/routers/matchesRouter.ts
+++ src/routers/matchesRouter.ts
@@ -6,7 +6,7 @@ import * as matchesController from "../controllers/matchesController.js"
 const matchesRouter = Router();
 
 matchesRouter.get("/", matchesController.get);
-matchesRouter.get("/:id", matchesController.get);
+matchesRouter.get("/:id", matchesController.getByUniqueId);
 matchesRouter.get("/duels/:id", matchesController.getMatch);
 
 
